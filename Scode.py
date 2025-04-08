9a4212d8909b95c4dcf0ecf38d28f7b076162e29
haddadi <haddadihanie@gmail.com>
2025-04-08 18:13:55 +0330

    add product list and show-product def to Scode.py

---
+++ Scode.py
@@ -0,0 +1,8 @@
+#product list 
+products= [{"id": 1, "name": "laptop", "price": 250, "stock": 6},
+{"id ": 2, "name": "camera", "price":300, "stock": 28}]
+
+def show_product():
+    print("/n product list")
+    for product in products:
+        print(f"id:{product["id"]}, name:{product["name"]}")
