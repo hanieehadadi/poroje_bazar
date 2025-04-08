7bffbaf4e826d77321a149e9e4c85e3ddf76ad7a
haddadi <haddadihanie@gmail.com>
2025-04-08 18:17:46 +0330

    add show-menue def to Scode.py

--- Scode.py
+++ Scode.py
@@ -2,6 +2,10 @@
 products= [{"id": 1, "name": "laptop", "price": 250, "stock": 6},
 {"id ": 2, "name": "camera", "price":300, "stock": 28}]
 
+
+def show_menue():
+    print("show all product")
+
 def show_product():
     print("/n product list")
     for product in products:
