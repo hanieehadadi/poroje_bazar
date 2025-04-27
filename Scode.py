72a4da5b2f6ac5570abda09058b27bd1c42cfe33
haddadi <haddadihanie@gmail.com>
2025-04-27 09:17:40 +0330

    i add new ithem  to dict in Scode python file

--- Scode.py
+++ Scode.py
@@ -1,7 +1,8 @@
 #product list 
 products= [{"id": 1, "name": "laptop", "price": 250, "stock": 6},
 {"id":4, "name": "Swatch", "price": 26, "stock":28}
-,{"id ": 2, "name": "camera", "price":300, "stock": 28}]
+,{"id ": 2, "name": "camera", "price":300, "stock": 28},
+{"id": 78, "name": "headphone", "price": 6700, "stock": 25}]
 
 
 def show_menue():
@@ -17,4 +18,4 @@ def show_product_tabel():
     df= pd.Dataframe(products)
     df['stockPrice']= df['cost']* ['stock']
     print("\n-- show tabel ---")
-    ptint(df)
+    print(df)
